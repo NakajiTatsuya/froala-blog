5f3f7ffea92c753a19afa67d3176bf4683ee05d7
nakajitatsuya <lwPJwwAe>
2019-03-06 11:52:09 +0900

    add smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,14 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "dragonarrow_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.smtp_settings = {
+    :user_name => ENV['username'],
+    :password => ENV['password'],
+    :address => 'smtp.mailtrap.io',
+    :domain => 'smtp.mailtrap.io',
+    :port => '2525',
+    :authentication => :cram_md5
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
