d4d42c5ef9538f55e14058c1cc840ab019edd3bd
nakajitatsuya <lwPJwwAe>
2019-03-08 12:27:43 +0900

    replace clearance with devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,12 +32,12 @@ Rails.application.configure do
   config.active_storage.service = :amazon
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :user_name => ENV['username'],
+    :user_name => ENV['user_name'],
     :password => ENV['password'],
     :address => 'smtp.mailtrap.io',
     :domain => 'smtp.mailtrap.io',
