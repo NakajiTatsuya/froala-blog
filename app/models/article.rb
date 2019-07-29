bb9b28f1492ff8c29e6f273c6fb69697a9fcc4f2
nakajitatsuya <lwPJwwAe>
2019-07-29 17:26:59 +0900

    add search

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,13 @@
 class Article < ApplicationRecord
 	has_one_attached :image
 	has_and_belongs_to_many :tags
+
+	def self.search(search) #self.でクラスメソッドとしている
+	    if search # Controllerから渡されたパラメータが!= nilの場合は、titleカラムを部分一致検索
+	    	Article.where(['title ILIKE ?', "%#{search}%"])
+	    else
+	        Article.all #全て表示。
+	    end
+    end
 	
 end
