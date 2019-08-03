8e842008b91a147563dae82daecaa8eced3705b2
nakajitatsuya <lwPJwwAe>
2019-08-03 15:21:43 +0900

    repair bug

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,8 @@
 class Tag < ApplicationRecord
 	has_and_belongs_to_many :articles
+
+    # articleが多い順に全てのtagを取得している
+	def self.popular_sort_tags
+	    Tag.select('tags.*', 'count(articles.id) AS acs').left_joins(:articles).group('tags.id').order('acs desc')
+    end
 end
