69ba4a5705dce0c39dc3f9e6d095ebd1020189c3
nakajitatsuya <lwPJwwAe>
2021-01-24 21:33:00 +0900

    AIタグの追加

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,7 +2,12 @@ class Tag < ApplicationRecord
 	has_and_belongs_to_many :articles
 
     # articleが多い順に全てのtagを取得している
-	def self.popular_sort_tags
-	    Tag.select('tags.*', 'count(articles.id) AS acs').left_joins(:articles).group('tags.id').order('acs desc')
+		# tag.allと記事数をselectし、記事数で降順に並び替え
+	  def self.popular_sort_tags
+		  Tag.select('tags.*', 'count(articles.id) AS num_of_articles').left_joins(:articles).group('tags.id').order('num_of_articles desc')
+	  end
+
+	  def self.popular_sort_tags_of_language
+	    Tag.select('tags.*', 'count(articles.id) AS num_of_articles').left_joins(:articles).group('tags.id').where(is_language: true).order('num_of_articles desc')
     end
 end
